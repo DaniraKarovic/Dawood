7801d2b3a30c6f505030034c1816116c2918f97c
DaniraKarovic <karovic.danira@gmail.com>
2017-07-22 23:09:05 +0200

    added user and admin part of page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
    root 'home#index'
    get "/contact" => "home#contact"
    resources :products
+   namespace :admin do
+     resources :products
+   end
+
 end
